3de327e525a77819445e232b9fd8395225df7587
Aurelien <a.couturier5863@gmail.com>
2016-05-10 19:14:05 +0200

    no message

---
+++ application/views/headerbarrenav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#"> ERDF </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="#">Lien1</a>
+                </li>
+                <li>
+                    <a href='../controle/create'>Lien2</a>
+                </li>
+                <li>
+                    <a href='../controle'>Liste Controle</a>
+                </li>
+                <li>
+                    <a href='../controle'>Statistiques</a>
+                </li>
+                <li>
+                    <a href='../controle'>Moulinette</a>
+                </li>
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" aria-expanded="true" aria-haspopup="true" role="button" data-toggle="dropdown" href="#">
+                        Dropdown exemple
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li> <a href='../controle/create'>Lien2</a></li>
+                    </ul>
+                </li>
+        </div>
+    </div>
+</nav>
